c009c7cb6a3bbc753e8a09708f6f88ec462f1a20
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-07-25 22:27:25 +0530

    Create read&show.py

---
+++ basic_command_opencv/read&show.py
@@ -0,0 +1,16 @@
+#to read and show the picture any where in the computer
+
+import cv2
+import numpy as np
+import  matplotlib.pyplot  as plt
+
+img=cv2.imread('D:\QQ.png',cv2.IMREAD_GRAYSCALE)
+cv2.imshow('image',img)
+k=cv2.waitKey(0)
+x=cv2.destroyAllWindows()
+print(k)
+print(x)
+plt.plot([50],[100],'c',Linewidth=5)
+plt.show()
+
+cv2.imwrite('q2.jpg',img)
